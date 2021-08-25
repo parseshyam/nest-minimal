5a5029cd9d01546a668ead0c9e3ea1786221838c
parse_shyam <parse.s@simformsolutions.com>
2021-08-25 16:48:28 +0530

    Added deployment shell script

--- deploy.js
+++ deploy.js
@@ -25,7 +25,9 @@ s.exec("tsc");
 s.echo(`3. SETTING UP ${CURR_ENV.toUpperCase()} ENVIROMENT...`);
 s.cp(`.${process.env.NODE_ENV}.env`, `build/.${process.env.NODE_ENV}.env`);
 
+s.echo(`4. SETTING UP ${CURR_ENV.toUpperCase()} LOCALES...`);
+s.cp("-rf", 'src/locales', 'build/src/locales');
 
-s.echo(`4. STARTING ${CURR_ENV.toUpperCase()} SERVER...`);
+s.echo(`5. STARTING ${CURR_ENV.toUpperCase()} SERVER...`);
 s.exec(`pm2 delete ${CURR_ENV.toUpperCase()}`);
-s.exec(`cross-env NODE_ENV=${CURR_ENV} pm2 start ./build/server.js --name ${CURR_ENV.toUpperCase()}`);
+s.exec(`cross-env NODE_ENV=${CURR_ENV} pm2 start build/src/main.js --name ${CURR_ENV.toUpperCase()}`);
