a1afecf93b1e61deb84f9b0fa92ad80b90c59bf4
parse_shyam <parse.s@simformsolutions.com>
2021-08-30 00:54:00 +0530

    Added cron services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
-import { UsersModule } from './modules/users/users.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AllModules } from './modules';
 import { KeysConfigModule } from './config/key.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { CronService } from "./loaders/cron/cron.service";
 
 @Module({
-  imports: [KeysConfigModule, UsersModule],
+  imports: [KeysConfigModule, ScheduleModule.forRoot(), ...AllModules],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CronService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/loaders/cron/cron.service.ts
@@ -0,0 +1,14 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+
+@Injectable()
+export class CronService {
+	private readonly logger = new Logger(CronService.name);
+
+	@Cron('45 * * * * *', {
+		name: 'clean-db'
+	})
+	handleCron() {
+		this.logger.debug('Called when the current second is 45');
+	}
+}
